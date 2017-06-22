ee563bfbdd790973f6a42e7e6209074c25aefe81
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-06-22 14:33:43 +0700

    request _id hits only when searching for ids

--- lib/elasticfusion/search.ex
+++ lib/elasticfusion/search.ex
@@ -6,18 +6,20 @@ defmodule Elasticfusion.Search do
   or `{:error, %HTTPoison.Response}`.
   """
   def find_ids(query, index) do
+    query = Map.put(query, :_source, false)
+    options = [filter_path: "hits.hits._id"]
+
     response = Elastix.Search.search(
       Application.get_env(:elasticfusion, :endpoint),
       index.index_name(),
       [index.document_type()],
-      query)
+      query, options)
 
     case response do
       {:ok, %HTTPoison.Response{body: %{"hits" => %{"hits" => hits}}}} ->
-        ids =
-          Enum.map(hits, fn(%{"_id" => id}) -> id end)
-
-        {:ok, ids}
+        {:ok, Enum.map(hits, fn(%{"_id" => id}) -> id end)}
+      {:ok, %HTTPoison.Response{body: %{}, status_code: 200}} ->
+        {:ok, []}
       error ->
         error
     end
